0264d5c93cbbbc3c46b40a6dfea519867d5aaeb4
Evelyn Avramuti <evelynav@edu.aau.at>
2024-03-06 22:00:31 +0100

    Added new class NetworkTCP for the network connection

---
+++ app/src/main/java/com/example/networktest/NetworkTCP.java
@@ -0,0 +1,10 @@
+package com.example.networktest;
+
+import android.os.AsyncTask;
+
+public class NetworkTCP extends AsyncTask<String, Void, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
