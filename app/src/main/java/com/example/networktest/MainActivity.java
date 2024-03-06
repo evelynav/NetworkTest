3c9735b1360b19bced99ac34d52f25b56189d48e
Evelyn Avramuti <evelynav@edu.aau.at>
2024-03-06 21:25:00 +0100

    Added edit text, button and input text with initialization

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -13,19 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText matrikelNr;
+    EditText txtMatrikelNr;
     Button btnSend;
-    TextView responseText;
+    TextView lblResponseServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtMatrikelNr = findViewById(R.id.txtMatrikelNr);
+        btnSend = findViewById(R.id.btnSend);
+        lblResponseServer = findViewById(R.id.lblResponseServer);
+
     }
 }
