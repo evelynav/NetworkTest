f94291954aa130749f3847c9f49436af9cf4760c
Evelyn Avramuti <evelynav@edu.aau.at>
2024-03-07 12:21:30 +0100

    Create Object MainActivity Added method onPostExecute()

--- app/src/main/java/com/example/networktest/NetworkServerTCP.java
+++ app/src/main/java/com/example/networktest/NetworkServerTCP.java
@@ -3,8 +3,14 @@ package com.example.networktest;
 import android.os.AsyncTask;
 
 public class NetworkServerTCP extends AsyncTask<String, Void, String> {
+
+    MainActivity mainActivity = new MainActivity();
     @Override
     protected String doInBackground(String... strings) {
         return null;
+    }
+
+    protected void onPostExecute (String result){
+        mainActivity.lblResponseServer.setText(result);
     }
 }
