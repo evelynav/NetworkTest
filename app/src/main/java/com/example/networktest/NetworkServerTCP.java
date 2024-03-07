b3e85a88aa468a62411d35f339b00869a01c6296
Evelyn Avramuti <evelynav@edu.aau.at>
2024-03-07 17:37:23 +0100

    Added sending data to the server

--- app/src/main/java/com/example/networktest/NetworkServerTCP.java
+++ app/src/main/java/com/example/networktest/NetworkServerTCP.java
@@ -3,7 +3,9 @@ package com.example.networktest;
 import android.os.AsyncTask;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -18,7 +20,9 @@ public class NetworkServerTCP extends AsyncTask<String, Void, String> {
             //Connect to the server
             Socket socket = new Socket("se2-submission.at", 20080);
 
-            //TODO Send data to server
+            //Send data to server
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            out.write("Hello Server");
 
             //Read response from server
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
