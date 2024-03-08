81b1cbcb839b42cd022c6b4edc651365fc8047c6
Evelyn Avramuti <evelynav@edu.aau.at>
2024-03-08 20:15:58 +0100

    Class AsyncTask is deprecated -> Method doInBackground() moved to MainActivity & deleted Class NetworkServerTCP

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -11,6 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText txtMatrikelNr;
@@ -32,5 +40,28 @@ public class MainActivity extends AppCompatActivity {
         btnSend = findViewById(R.id.btnSend);
         lblResponseServer = findViewById(R.id.lblResponseServer);
 
+    }
+
+    protected String doInBackground(String... strings) {
+        try{
+            //Connect to the server
+            Socket socket = new Socket("se2-submission.at", 20080);
+
+            //Send data to server
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            out.write("Hello Server");
+
+            //Read response from server
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String receivedMessage = in.readLine();
+
+            //Close connection
+            socket.close();
+        } catch (UnknownHostException err){
+            return "Unknown Host: " + err.getMessage();
+        } catch (IOException err) {
+            return "IO Exception: " + err.getMessage();
+        }
+        return null;
     }
 }
