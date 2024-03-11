f4508abbfa4c3daa151401f151e51d22b4abed27
Evelyn Avramuti <evelynav@edu.aau.at>
2024-03-11 09:04:32 +0100

    Changed OutputStream()

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -13,10 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -53,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
                 //Connect to the server
                 Socket socket = new Socket("se2-submission.aau.at", 20080);
 
-                    //Send data to server
-                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    out.write(txtMatNrInput.getText().toString());
+                //Send data to server
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                printWriter.println(txtMatNrInput.getText().toString());
+                printWriter.flush();
 
                 //Read response from server
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
